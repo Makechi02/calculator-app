d485185bfa3ee239d9b9cf4335a88d0b2e938b28
Makechi <makechieric9@gmail.com>
2024-05-13 14:37:23 +0300

    remove react-buddy

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.js";
 import {ThemeProvider} from "./context/ThemeContext.jsx";
 import {ExpressionProvider} from "./context/ExpressionContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
-			<ThemeProvider>
-				<ExpressionProvider>
-					<App/>
-				</ExpressionProvider>
-			</ThemeProvider>
-		</DevSupport>
+		<ThemeProvider>
+			<ExpressionProvider>
+				<App/>
+			</ExpressionProvider>
+		</ThemeProvider>
 	</React.StrictMode>,
 )
